14e6640c264bf4c66688e57866db7a77a1374eff
cycjimmy <cycjimmy@gmail.com>
2018-08-16 10:28:58 +0800

    180816 use propTypes

--- src/Title.js
+++ src/Title.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const Title = (props) => {
   const handleClickOnTitle = (event) => console.log('Hello World');
   return (
     <h1 onClick={handleClickOnTitle}>{props.title}</h1>
   );
+};
+
+Title.propTypes = {
+  title: PropTypes.string
 };
 
 export default Title;
